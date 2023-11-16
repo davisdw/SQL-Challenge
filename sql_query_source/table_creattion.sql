402c8c1cc400a9c10b2ea896c62f04f28364d069
davisdw <ddavis85@Dwaynes-MBP.attlocal.net>
2023-11-16 10:20:43 -0500

    Update

--- sql_query_source/table_creattion.sql
+++ sql_query_source/table_creattion.sql
@@ -7,46 +7,50 @@
 	DROP TABLE IF EXISTS dept_mgr;
 	
 
-
 --create employee table
 	
 	CREATE TABLE employee(
 					  emp_no INT PRIMARY KEY NOT NULL,
 					  emp_title_id VARCHAR(5) NOT NULL, 
-					  birth_date DATE NOT NULL,
+					  birth_date VARCHAR(255) NOT NULL,
 					  first_name VARCHAR(255),
 					  last_name VARCHAR(255),
 					  sex CHAR NOT NULL,
 					  hire_date DATE NOT NULL
-					  --FOREIGN KEY (emp_title_id) REFERENCES job_title(title_id)
 					  );
 					  
 					  
-SELECT * FROM employee
+	SELECT * FROM employee
 
 					  
 --Section where insert the live data into this query schema
 
 --Create Job Title, Salaries Department tables
 
+	CREATE TABLE salaries(
+					emp_no INT PRIMARY KEY,
+					salary MONEY,
+					FOREIGN KEY(emp_no) REFERENCES employee(emp_no)
+					);
+	
+	SELECT * FROM salaries
+	
+	
 	CREATE TABLE job_title(
 					  title_id VARCHAR(5) NOT NULL,
 					  title VARCHAR(30),
 					  PRIMARY KEY (title_id)
 					  );
-
-	CREATE TABLE salaries(
-					emp_no INT PRIMARY KEY,
-					salary INT,
-					FOREIGN KEY(emp_no) REFERENCES employee(emp_no)
-					);
-					
+					  
+	SELECT * FROM job_title
 					
 	CREATE TABLE department(
 					dept_no VARCHAR(4) NOT NULL,
 					dept_name VARCHAR(50) NOT NULL,
 					PRIMARY KEY(dept_no)
 					);
+					
+	SELECT * FROM department
 	
 --Create junction tables Department_Employees and Department_Managers
 
@@ -56,6 +60,8 @@ SELECT * FROM employee
 				  FOREIGN KEY (emp_no) REFERENCES employee(emp_no),
 				  FOREIGN KEY (dept_no) REFERENCES department(dept_no)
 				  );
+				  
+	SELECT * FROM dept_emp
 	
 	CREATE TABLE dept_mgr(
 				 dept_no VARCHAR(4) NOT NULL,
@@ -63,4 +69,5 @@ SELECT * FROM employee
 				 FOREIGN KEY (dept_no) REFERENCES department(dept_no),
 				 FOREIGN KEY (emp_no) REFERENCES employee(emp_no)
 				 );
-
+	
+	SELECT * FROM dept_mgr
